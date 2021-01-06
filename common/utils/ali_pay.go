08f77266cdc4c1f73bcbf6eddf65350c7a628444
shijiang <guoshijiang2012@163.com>
2021-01-06 20:15:51 +0800

    fix

--- common/utils/ali_pay.go
+++ common/utils/ali_pay.go
@@ -3,7 +3,7 @@ package utils
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
-	"github.com/smartwalle/alipay/v3"
+	alipay_zfb "github.com/smartwalle/alipay/v3"
 )
 
 
@@ -12,17 +12,18 @@ func AliPayZfb(notify_url, return_url, order_number, pay_amount string) string {
 	privateKey := beego.AppConfig.String("ali_private_key")
 	appId := beego.AppConfig.String("ali_app_id")
 	//aliPublicKey :=  beego.AppConfig.String("public_key")
-	client, err := alipay.New(appId, privateKey, true)
+	client, err := alipay_zfb.New(appId, privateKey, true)
 	if err != nil {
 		logs.Error(err.Error())
 	}
-	var p = alipay.TradeAppPay{}
+	var p = alipay_zfb.TradeAppPay{}
 	p.NotifyURL = notify_url
 	p.ReturnURL = return_url
 	p.Subject = beego.AppConfig.String("ali_project_name")
 	p.OutTradeNo = order_number
 	p.TotalAmount = pay_amount
 	p.ProductCode = "FAST_INSTANT_TRADE_PAY"
+	p.TimeExpire = "2021-12-28 00:00"
 	url, err := client.TradeAppPay(p)
 	if err != nil {
 		logs.Error(err.Error())

a6f1dfd60918dae8259349fa984483de2de48608
shijiang <guoshijiang2012@163.com>
2021-01-08 14:36:28 +0800

    fix crt

--- common/utils/ali_pay.go
+++ common/utils/ali_pay.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	alipay_zfb "github.com/smartwalle/alipay/v3"
@@ -15,7 +16,6 @@ func AliPayZfb(notify_url, return_url, order_number, pay_amount string) string {
 	if err != nil {
 		logs.Error(err.Error())
 	}
-
 	var p = alipay_zfb.TradeAppPay{}
 	p.NotifyURL = notify_url
 	p.ReturnURL = return_url
@@ -24,16 +24,9 @@ func AliPayZfb(notify_url, return_url, order_number, pay_amount string) string {
 	p.TotalAmount = pay_amount
 	p.ProductCode = "FAST_INSTANT_TRADE_PAY"
 	if client != nil {
-		err = client.LoadAliPayPublicCertFromFile("./gingernet.vip.csr")
-		if err != nil {
-			logs.Error("load crt fail")
-			return ""
-		}
-		err = client.LoadAliPayPublicKey(beego.AppConfig.String("ali_public_key"))
-		if err != nil {
-			logs.Error("load public key fail")
-			return ""
-		}
+		fmt.Println("加载证书 1:", client.LoadAppPublicCertFromFile("../../crt/appCertPublicKey_2021002116677316.crt"))
+		fmt.Println("加载证书 2:", client.LoadAliPayRootCertFromFile("../../crt/alipayRootCert.crt"))
+		fmt.Println("加载证书 3:", client.LoadAliPayPublicCertFromFile("../../crt/alipayCertPublicKey_RSA2.crt"))
 		url, err := client.TradeAppPay(p)
 		if err != nil {
 			logs.Error(err.Error())

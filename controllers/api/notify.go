04f3a3f90e86d0953404d00f0a89e93731984416
shijiang <guoshijiang2012@163.com>
2021-01-09 19:19:07 +0800

    fix notify

--- controllers/api/notify.go
+++ controllers/api/notify.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+	"ganji/types"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 )
@@ -14,8 +16,17 @@ type NotifyController struct {
 // @Description 支付支付成功回调函数 ZhifubaoNotify
 // @Success 200 status bool, data interface{}, msg string
 // @router /zfb_notify [post]
-func (this *UserController) ZhifubaoNotify() {
-	logs.Info("========zfb_notifyzfb_notifyzfb_notify==========")
-	logs.Info(this.Ctx.Input.RequestBody)
-	logs.Info("========zfb_notifyzfb_notifyzfb_notify==========")
+func (this *NotifyController) ZhifubaoNotify() {
+	logs.Info("========body==========")
+	var zhifubao_param interface{}
+	if err := json.Unmarshal(this.Ctx.Input.RequestBody, &zhifubao_param); err != nil {
+		this.Data["json"] = RetResource(false, types.InvalidFormatError, err.Error(), "无效的参数格式,请联系客服处理")
+		this.ServeJSON()
+		return
+	}
+	logs.Info(zhifubao_param)
+	logs.Info("========body==========")
+	this.Data["json"] = RetResource(true, 200, nil, "success")
+	this.ServeJSON()
+	return
 }

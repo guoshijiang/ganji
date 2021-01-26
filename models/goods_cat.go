8f1fd9e560ad1768fbfd621a150ef21c80b4367b
shijiang <guoshijiang2012@163.com>
2021-01-26 22:33:59 +0800

    fix

--- models/goods_cat.go
+++ models/goods_cat.go
@@ -15,7 +15,7 @@ type GoodsCat struct {
 	FatherCatId  int64     `json:"father_cat_id"`              // 父级分类 ID
 	Name         string    `orm:"size(512);index" json:"name"` // 分类标题
 	Icon         string    `orm:"size(150);default(/static/upload/default/user-default-60x60.png)" json:"icon"` // 分类Icon
-	IsDispay     int8      `orm:"default(0)" json:"is_dispay"`   // 0 不显示 1 显示
+	IsDispay     int8      `orm:"default(0)" json:"is_dispay"`   // 0 显示 1 不显示
 }
 
 func (this *GoodsCat) TableName() string {
